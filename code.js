4bfb0c50c69f9873f3de59fe87e879c8a4aeb19e
Alex <154551635+Alex200027fr@users.noreply.github.com>
2024-11-02 18:09:08 +0100

    Add function humanChoice

--- code.js
+++ code.js
@@ -1,6 +1,19 @@
-const choices =['rock', 'paper', 'cissors'];
+const choices =['rock', 'paper', 'Scissors'];
+let humanChoice = prompt('Rock, paper or scissors ?')
 
         function getComputerChoice(){
             let randomNumber = Math.floor(Math.random()*3);
             return (choices[randomNumber]);
+        }
+
+        function getHumanChoice(){
+            if (humanChoice.toLowerCase() === 'rock'){
+                console.log('rock')
+            } else if (humanChoice.toLowerCase() === 'paper'){
+                console.log('paper')
+            } else if (humanChoice.toLowerCase() === 'scissors'){
+                console.log('scissors')
+            } else if (humanChoice !== 'rock' || 'paper' || 'scissors'){
+                console.log('Please choose a sign between \'Rock\',\'Paper\' or \'Scissors\'.')
+            }
         }
